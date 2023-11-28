7e60a4e250466efc3ba408e530ac87f4c7b6de9b
Clark Le <lenguyenhunganh2001@gmail.com>
2023-11-28 23:08:49 +0700

    Add conf

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -9,5 +12,6 @@ def index(request):
     return render(request, "pages/index.html")
 
 
+@api_view(("GET",))
 def health_check(request):
-    return "ok"
+    return Response(data="ok", status=status.HTTP_200_OK)
