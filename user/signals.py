f4c1d300df77fab44d2b6b69eec02262528c3056
waflol <65594745+waflol@users.noreply.github.com>
2023-11-28 23:39:31 +0700

    Remove comment (#4)

--- user/signals.py
+++ user/signals.py
@@ -7,31 +7,6 @@ from django.dispatch import receiver
 from user.models import User
 
 
-# _OLD_FILEFIELD = 'old_filefield'
-#
-#
-# @receiver(pre_save, sender=User)
-# def skip_delete_old_image(sender, instance=None, **kwargs):
-#     try:
-#         old_instance = User.objects.get(pk=instance.pk)
-#     except User.DoesNotExist:
-#         return False
-#
-#     if old_instance.avatar.name.split('/')[0] != 'default' and old_instance.avatar != instance.avatar:
-#         setattr(instance, _OLD_FILEFIELD, old_instance.avatar)
-#
-#     if old_instance.cover.name.split('/')[0] != 'default' and old_instance.cover != instance.cover:
-#         setattr(instance, _OLD_FILEFIELD, old_instance.cover)
-#
-#
-# @receiver(post_save, sender=User)
-# def delete_old_image(sender, instance, created, **kwargs):
-#     if created:  # Skip if instance is being created
-#         return
-#     if hasattr(instance, _OLD_FILEFIELD):
-#         old_image = getattr(instance, _OLD_FILEFIELD)
-#         if os.path.isfile(old_image.path):
-#             default_storage.delete(old_image.path)
 @receiver(post_save, sender=User)
 def add_default_image(sender, instance, created, **kwargs):
     if (
